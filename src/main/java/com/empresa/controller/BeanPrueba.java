ba4fa47f65ebf1aa4f6019bdcb37ce0021c8126b
Juan Pablo Casallas Ahumada <casallasahumadajuanpablo@gmail.com>
2026-09-23 22:23:43 -0500

    aspecto visual de vistas completo, clases completas

--- src/main/java/com/empresa/controller/BeanPrueba.java
+++ src/main/java/com/empresa/controller/BeanPrueba.java
@@ -14,6 +14,6 @@ public class BeanPrueba implements Serializable {
 	private static final long serialVersionUID = -7704795523440040251L;
 
 	public String mensaje() {
-		return "Calculo de CDT---";
+		return "---Calculo de CDT--- BANCO DIBIDIBI S.A ---";
 	}
 }
